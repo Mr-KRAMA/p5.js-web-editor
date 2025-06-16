fb87357d37e7ad039179e558f3960aa60a7daeaa
raclim <43053081+raclim@users.noreply.github.com>
2025-06-16 17:22:11 -0400

    move to right place

--- server/domain-objects/createDefaultFiles.js
+++ server/domain-objects/createDefaultFiles.js
@@ -1,9 +1,8 @@
 export const defaultSketch = `function setup() {
   createCanvas(400, 400);
+  // Help blind or low-vision users using screen readers understand your sketch.
+  describe("A 400 by 400 pixel gray canvas.");
 }
-
-// Help blind or low-vision users using screen readers understand your sketch.
-describe("A 400 by 400 pixel gray canvas.");
 
 function draw() {
   background(220);
